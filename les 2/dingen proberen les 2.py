cb3030c0466cf0423e51eece21bd95ed1c0580c1
AidanMiddel <aidanm@ctjies.nl>
2020-09-22 14:24:28 +0200

    les 2

---
+++ les 2/dingen proberen les 2.py
@@ -0,0 +1,7 @@
+#doel : voor en achternaam kunnne invoeren de computer weergeeft mijn volledige naam
+
+print("Hallo, wat is jou voornaam?")
+naam = input()
+print("En wat is jou achternaam?")
+achternaam = input()
+print("Hallo, " + naam + " " + achternaam + " aangenaam kennis te maken")
---
+++ les 2/les2.py
@@ -0,0 +1,24 @@
+naam = "Aidan" #je eigen naam (string)
+aanuit = True #of iets waar of niet waar is (dus true or false) (boolean)
+kommagetal = 5.5 #komma getal (float of double)
+karakter = '#' #een symbool (character)
+heelgetal = 123 #een getal (interger)
+
+kommagetal = kommagetal * 2
+
+restwaarde = kommagetal % 3
+
+print(naam)
+print(aanuit)
+print(kommagetal)
+print(karakter)
+print(heelgetal)
+print(restwaarde)
+
+
+uitkomst = heelgetal / kommagetal
+print(uitkomst)
+
+stukjetekst = "hallo"
+stukjetekst += " mijn naam is " + naam
+print(stukjetekst)
